661ae2d89d0041767833e08ccfdde139a38dfdd3
Koori-Cc <panbing930106@qq.com>
2017-09-19 10:13:49 +0800

    2017/9/19  10:00  update

--- src/main/java/com/zk/ssm_demo/controllers/KendoController.java
+++ src/main/java/com/zk/ssm_demo/controllers/KendoController.java
@@ -3,16 +3,15 @@ package com.zk.ssm_demo.controllers;
 import com.zk.ssm_demo.entities.PaginationVO;
 import com.zk.ssm_demo.entities.User;
 import com.zk.ssm_demo.services.UserService;
-import org.apache.commons.collections.map.HashedMap;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 /**
  * @author panbing@supcon.com
@@ -89,5 +88,49 @@ public class KendoController {
         List<String> list = userService.queryAddress();
         return list;
     }
+
+    /**
+     * 上传图片
+     * @param file
+     * @return
+     */
+    @RequestMapping(value="/upload.do",method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String, Object> upload(MultipartFile file) {    //返回值的类型一定要对应!!不能写Object,否则前台接收数据会出问题
+        Map<String, Object> map = new HashMap<String, Object>();
+        String filename = "";
+        boolean result = false;
+        if(!file.isEmpty()) {   //说明上传了文件
+            ResourceBundle bundle = ResourceBundle.getBundle("config");
+            String path = bundle.getString("nginx.imgPath");
+            File storePath = new File(path);
+            if(!storePath.exists()) {
+                storePath.mkdirs();
+            }
+            filename = file.getOriginalFilename();
+            //按照一定的规则对文件进行重新命名,常用UUID
+            String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+            //取出原来的扩展名
+            String extName = filename.substring(filename.lastIndexOf("."));
+            //重新命名文件的名称
+            filename = uuid + extName;
+            //把文件保存到服务器
+            File storeDir = new File(path, filename);
+            try {
+                //将文件上传到指定的目录
+                file.transferTo(storeDir);
+                result = true;
+            } catch (IOException e) {
+                logger.info("文件上传失败!");
+                e.printStackTrace();
+                result = false;
+            }
+        }
+        map.put("success",result);
+        map.put("filename",filename);
+        return map;
+    }
+
+
 
 }

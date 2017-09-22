193b95f632f37e0ff64e7c08a7f1098dd4e43f0a
Koori-Cc <panbing930106@qq.com>
2017-09-22 14:35:08 +0800

    2017/9/22  14:30  update

---
+++ src/main/java/com/zk/ssm_demo/controllers/ProfileController.java
@@ -0,0 +1,40 @@
+package com.zk.ssm_demo.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author panbing@supcon.com
+ * @create 2017/9/21 10:02
+ * @description
+ */
+@Controller("ssm_demo_profile_controller")   //这相当于controller的名字
+@RequestMapping("/profile")
+public class ProfileController {
+
+    private Logger logger = LoggerFactory.getLogger(ProfileController.class);
+
+    @Value("${guitar_color}")
+    private String guitarColor;
+
+    @Value("${pick_color}")
+    private String pickColor;
+
+    @Value("${jdbc.username}")
+    private String name;
+
+    @RequestMapping("/print.do")
+    @ResponseBody
+    //@Scheduled(cron="0/5 * *  * * ? ")   //每5秒执行一次
+    public void print() {
+        logger.info("********************" + guitarColor + "********************");
+        logger.info("********************" + pickColor + "********************");
+        logger.info("********************" + name + "********************");
+    }
+
+}
